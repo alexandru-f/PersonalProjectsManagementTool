844446e28915b0f177e0a9e402d614d95b7648a7
Alexandru F | Heimdal™ <afa@heimdalsecurity.com>
2019-06-03 16:48:14 +0300

    react

---
+++ src/main/resources/static/precache-manifest.cdfbe44a6444d8188bfed88e42ee2bc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fa16432d1fbacdfe5571",
+    "url": "/static/js/main.fa16432d.chunk.js"
+  },
+  {
+    "revision": "1ad2a0d260d5e511f09f",
+    "url": "/static/js/1.1ad2a0d2.chunk.js"
+  },
+  {
+    "revision": "fa16432d1fbacdfe5571",
+    "url": "/static/css/main.a3ffcee9.chunk.css"
+  },
+  {
+    "revision": "1ad2a0d260d5e511f09f",
+    "url": "/static/css/1.440f6202.chunk.css"
+  },
+  {
+    "revision": "3be033365b5f0d6cb57ec3fd0333c5cf",
+    "url": "/index.html"
+  }
+];
