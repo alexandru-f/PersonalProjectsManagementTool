4f5f1319045737c9bb9b206e2a318ada2e148fd9
Alexandru F | Heimdal™ <afa@heimdalsecurity.com>
2019-06-04 23:01:27 +0300

    added antmatcher to match resources

--- src/main/java/io/alexandru/ppmtool/Security/SecurityConfig.java
+++ src/main/java/io/alexandru/ppmtool/Security/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers(    "/",
-                        "/favicon.ico",
+                        "/resources/static/**",
                         "/**/*.png",
                         "/**/*.gif",
                         "/**/*.svg",
