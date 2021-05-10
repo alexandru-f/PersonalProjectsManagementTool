5ffd1481574e8f9f58f3d015d55dd6611b3036f6
alex <afa@heimdalsecurity.com>
2021-05-10 13:26:24 +0300

    added new app props

--- src/main/java/io/alexandru/ppmtool/Security/SecurityConfig.java
+++ src/main/java/io/alexandru/ppmtool/Security/SecurityConfig.java
@@ -69,6 +69,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers(    "/",
+                        "/resources/static/**",
                         "/**/*.png",
                         "/**/*.gif",
                         "/**/*.svg",
