11c2fd1d31d5209c9a7f8c2117bd37b8b2abdbd8
alex <afa@heimdalsecurity.com>
2021-05-10 17:47:27 +0300

    added smtp configs

--- src/main/java/io/alexandru/ppmtool/Security/SecurityConfig.java
+++ src/main/java/io/alexandru/ppmtool/Security/SecurityConfig.java
@@ -69,7 +69,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers(    "/",
-                        "/resources/static/**",
                         "/**/*.png",
                         "/**/*.gif",
                         "/**/*.svg",
