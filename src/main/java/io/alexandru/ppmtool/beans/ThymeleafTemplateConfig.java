453caf2666e301b3e4f59afb8a501823c6f62fe1
alexandru-f <fasoleac.alexandru@gmail.com>
2021-05-24 17:53:18 +0300

    Create ThymeleafTemplateConfig.java

---
+++ src/main/java/io/alexandru/ppmtool/beans/ThymeleafTemplateConfig.java
@@ -0,0 +1,18 @@
+@Configuration
+public class ThymeleafTemplateConfig {
+    @Bean
+    public SpringTemplateEngine springTemplateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.addTemplateResolver(htmlTemplateResolver());
+        return templateEngine;
+    }
+    @Bean
+    public SpringResourceTemplateResolver htmlTemplateResolver(){
+        SpringResourceTemplateResolver emailTemplateResolver = new SpringResourceTemplateResolver();
+        emailTemplateResolver.setPrefix("/templates/");
+        emailTemplateResolver.setSuffix(".html");
+        emailTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        emailTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        return emailTemplateResolver;
+    }
+}
